bc774a65216f3e649a1442b00ac9b3beb34da819
Nick Schefner <124909285+ItsJuzoSuzuya@users.noreply.github.com>
2023-06-03 19:07:56 +0200

    Topics add functionality

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ function navigateToNewSite(url) {
 function saveText(button){
     var buttonText = button.innerText.trim();
 
-    var storedLanguages = localStorage.getItem('languages');
-    var storedValues = storedLanguages ? JSON.parse(storedLanguages) : [];
+    var storedText = localStorage.getItem('languages');
+    var storedValues = storedLanguages ? JSON.parse(storedText) : [];
 
     storedValues.push(buttonText);
 
     localStorage.setItem('languages', JSON.stringify(storedValues));
+} 
+
+function saveText(input){
+  var inputText = input.value.trim();
+
+  var storedText = localStorage.getItem('topics');
+  var storedValues = storedText ? JSON.parse(storedText) : [];
+
+  storedValues.push(inputText);
+
+  localStorage.setItem('topics', JSON.stringify(storedValues));
 }
 
 function loadLanguageButtons() {
@@ -25,10 +36,31 @@ function loadLanguageButtons() {
         newButton.className = 'button languageButton';
         newButton.innerText = buttonText;
         newButton.onclick = function() {
-          navigateToNewSite('languageSite.html');
+          navigateToNewSite('topicSite.html');
         };
         addButton.parentNode.insertBefore(newButton, addButton);
       });
     }
   }
+
+function loadTopics(){
+  console.log("ther are topics");
+  var topics = localStorage.getItem('topics');
+
+  if (topics) {
+    console.log("ther are topics");
+    var addButton = document.querySelector('.addButton');
+    var savedValues = JSON.parse(topics);
+
+    savedValues.forEach(function(buttonText) {
+      var newButton = document.createElement('button');
+      newButton.className = 'button topicButton';
+      newButton.innerText = buttonText;
+      newButton.onclick = function() {
+        navigateToNewSite('topicSite.html');
+      };
+      addButton.parentNode.insertBefore(newButton, addButton);
+    });
+  }
+}
   
