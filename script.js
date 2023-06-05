65a4e50c756d9ca571f3b0c2c6a786d98687ca1c
Nick Schefner <124909285+ItsJuzoSuzuya@users.noreply.github.com>
2023-06-05 21:20:13 +0200

    Added Words to Topics

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function navigateToNewSite(url) {
   var currentLanguage;
   var urlParams = new URLSearchParams(window.location.search);
       currentLanguage = urlParams.get('lang');
+      currentTopic = urlParams.get('topic');
 
   if (window.event.target.classList.contains('languageButton')) {
     currentLanguage = window.event.target.innerText;
     window.location.href = url + '?lang=' + encodeURIComponent(currentLanguage);
-  } else if(currentLanguage != null){
+  }else if(window.event.target.classList.contains('topicButton')){
+    currentTopic = window.event.target.innerText;
+    window.location.href = url + '?lang=' + encodeURIComponent(currentLanguage) + '&topic=' + encodeURIComponent(currentTopic);
+  }else if(currentTopic != null){
+    window.location.href = url + '?lang=' + encodeURIComponent(currentLanguage) + '&topic=' + encodeURIComponent(currentTopic);
+  }else if(currentLanguage != null){
     window.location.href = url + '?lang=' + encodeURIComponent(currentLanguage);
   } else {
     window.location.href = url;
@@ -44,7 +50,7 @@ function loadLanguageButtons() {
     }
   }
 
-  function saveInputText(input){
+  function saveTopic(input){
     var urlParams = new URLSearchParams(window.location.search);
     var currentLanguage = urlParams.get('lang');
 
@@ -58,27 +64,78 @@ function loadLanguageButtons() {
     localStorage.setItem(currentLanguage + 'Topics', JSON.stringify(storedValues));
   }
 
+  function saveWord(input1, input2){
+    var urlParams = new URLSearchParams(window.location.search);
+    var currentLanguage = urlParams.get('lang');
+    var currentTopic = urlParams.get('topic');
+
+    var foreignWord = input1.value.trim();
+    var translation = input2.value.trim();
+  
+    var storedText = localStorage.getItem(currentLanguage +  currentTopic + 'Words');
+    var storedValues = storedText ? JSON.parse(storedText) : {};
+
+    storedValues[foreignWord] = translation;
+  
+    localStorage.setItem(currentLanguage + currentTopic + 'Words', JSON.stringify(storedValues));
+  }
+
 function loadTopics(){
   var urlParams = new URLSearchParams(window.location.search);
   var currentLanguage = urlParams.get('lang');
   var topics = localStorage.getItem(currentLanguage + 'Topics');
 
-  console.log(currentLanguage);
-  console.log(topics);
-
   if (topics) {
     var addButton = document.querySelector('.addButton');
     var savedValues = JSON.parse(topics);
 
-    savedValues.forEach(function(buttonText) {
+    savedValues.forEach(function(topic) {
       var newButton = document.createElement('button');
       newButton.className = 'button topicButton';
-      newButton.innerText = buttonText;
+      newButton.innerText = topic;
       newButton.onclick = function() {
-        navigateToNewSite('topicSite.html');
+        navigateToNewSite('wordSite.html');
       };
       addButton.parentNode.insertBefore(newButton, addButton);
     });
   }
+}
+
+  function loadWords(){
+    var urlParams = new URLSearchParams(window.location.search);
+    var currentLanguage = urlParams.get('lang');
+    var currentTopic = urlParams.get('topic');
+
+    var topics = localStorage.getItem(currentLanguage + currentTopic + 'Words');
+
+    //console.log(topics);
+  
+    if (topics) {
+      var addButton = document.querySelector('.addButton');
+      var savedValues = JSON.parse(topics);
+  
+      Object.entries(savedValues).forEach(function([key, value]) {
+        console.log(key + value);
+
+        var newButton = document.createElement('button');
+        newButton.className = 'button wordButton';
+
+        var upperText = document.createElement('span');
+        upperText.className = 'upperText';
+        upperText.innerText = key;
+      
+        var lowerText = document.createElement('span');
+        lowerText.className = 'lowerText';
+        lowerText.innerText = value;
+
+        newButton.appendChild(upperText);
+        newButton.appendChild(lowerText);
+
+        newButton.onclick = function() {
+          navigateToNewSite('wordSite.html');
+        };
+        addButton.parentNode.insertBefore(newButton, addButton);
+      });
+    }
 }
   
