afe5f44f8f360ebbc96736cafce24fc44f2132c7
Nick Schefner <124909285+ItsJuzoSuzuya@users.noreply.github.com>
2023-06-08 22:36:15 +0200

    node.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const http = require('http')
+const fs = require('fs')
+const port = 5500
+
+const server = http.createServer(function(req, res){
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  fs.readFile('/index.html', function(error, data){
+    if(error){
+      res.writeHead(404)
+      res.write('Error: File Not Found')
+    } else {
+      res.write(data)
+    }
+    res.end()
+  })
+})
+
+server.listen(port, function(error){
+  if(error){
+    console.log('Something went wrong', error)
+  } else {
+    console.log('Server is listening on port ', port)
+  }
+})
+
 function navigateToNewSite(url) {
   var currentLanguage;
   var urlParams = new URLSearchParams(window.location.search);
@@ -138,4 +163,3 @@ function loadTopics(){
       });
     }
 }
-  
